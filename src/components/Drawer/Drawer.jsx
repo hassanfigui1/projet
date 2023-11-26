b173083c6d24cdca52feaf24296245e9a5840b75
hassan figuigui <hassan.figuigui7@aiesec.net>
2023-11-26 20:50:51 +0100

    replacing code with existed List Component

--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -1,15 +1,8 @@
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import MailIcon from '@mui/icons-material/Mail';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
 import Divider from '@mui/material/Divider';
 import MuiDrawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
 import { styled, useTheme } from '@mui/material/styles';
 import * as React from 'react';
 import SharedList from '../../shared/components/list/List';
@@ -80,30 +73,8 @@ export default function DrawerSide({
         </IconButton>
       </DrawerHeader>
       <Divider />
-      <List>
-        {listOfSideItems.map((text, index) => (
-          <ListItem key={text} disablePadding sx={{display: 'block'}}>
-            <ListItemButton
-              sx={{
-                minHeight: 48,
-                justifyContent: open ? 'initial' : 'center',
-                px: 2.5,
-              }}>
-              <ListItemIcon
-                sx={{
-                  minWidth: 0,
-                  mr: open ? 3 : 'auto',
-                  justifyContent: 'center',
-                }}>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} sx={{opacity: open ? 1 : 0}} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+      <SharedList otherItemsSide={listOfSideItems} openState={openState} />
       <Divider />
-
       <SharedList otherItemsSide={otherItemsSide} openState={openState} />
     </Drawer>
   );
